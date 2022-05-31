674a3b7473aba6e353876ea27dc6668ed1ff047f
Kristina <codingkris@yahoo.com>
2022-05-31 22:55:48 +0300

    fix: add request parameters

--- source/js/getData.js
+++ source/js/getData.js
@@ -2,10 +2,16 @@
 let count = 15;
 let apiUrl = `https://api.thecatapi.com/v1/images/search?limit=${count}`;
 
+const requestParameters = {
+  headers: {
+    'x-api-key': '041355a5-6f53-45d5-b984-900a93bf2514',
+  },
+};
+
 // Get Cat Images from Cat API
 export default async function getData() {
   try {
-    const response = await (await fetch(apiUrl)).json();
+    const response = await (await fetch(apiUrl, requestParameters)).json();
     return response;
   } catch (error) {
     // Catch Error Here
